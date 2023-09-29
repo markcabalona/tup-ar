2ed1fbc958c36ae26bb60645effa4b8271ae71bb
Mark Cabalona <markcabalona58@gmail.com>
2023-09-30 01:30:28 +0800

    #1: set MaterialApp.theme values to AppTheme values

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tup_ar/core/theme/app_theme.dart';
 import 'package:tup_ar/features/Authentication/presentation/pages/registration_page.dart';
 
 void main() {
@@ -13,8 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TUP AR',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
+        inputDecorationTheme: AppTheme.inputDecorationTheme,
+        filledButtonTheme: AppTheme.filledButtonTheme,
       ),
       home: const RegistrationPage(),
     );
