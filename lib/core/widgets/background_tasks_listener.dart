5b7e3a4407283b253a4b5cfd31d4a4a1092fa364
Mark Cabalona <markcabalona58@gmail.com>
2023-10-01 23:36:05 +0800

    #3: #4: add BackGroundTasksListener widget

---
+++ lib/core/widgets/background_tasks_listener.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tup_ar/core/cubits/background_tasks_cubit.dart';
+
+class BackGroundTasksListener extends StatelessWidget {
+  final Widget child;
+  const BackGroundTasksListener({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<BackgroundTasksCubit, BackgroundTasksState>(
+      listener: (context, state) {
+        if (state is BackgroundTaskLoading) {
+          // TODO: show loading UI
+        } else if (state is BackgroundTaskSuccess) {
+          // TODO: show success UI
+        } else if (state is BackgroundTaskFailed) {
+          // TODO: show error UI
+        }
+      },
+      child: child,
+    );
+  }
+}
