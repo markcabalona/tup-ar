d9b4e7ce1a44569e9ff36ee0d37aa16fea1bfc85
Mark Cabalona <markcabalona58@gmail.com>
2023-10-01 23:15:09 +0800

    #5: add ErrorMessageConstants

---
+++ lib/core/constants/error_message_constants.dart
@@ -0,0 +1,4 @@
+abstract class ErrorMessageConstants {
+  static const serverError =
+      "We're sorry! The server has encountered an internal error and was unable to complete your request. Please try again later";
+}
