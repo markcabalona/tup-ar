4d19fd753be33b3b57591238b1f98c409963ff59
Mark Cabalona <markcabalona58@gmail.com>
2024-02-28 00:54:16 +0800

    faqs: add navigate-to-faqs-button in dashboard

--- lib/features/Authentication/presentation/widgets/authenticated/dashboard_drawer.dart
+++ lib/features/Authentication/presentation/widgets/authenticated/dashboard_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:tup_ar/core/router/app_router.dart';
+import 'package:tup_ar/core/router/routes/app_routes.dart';
 import 'package:tup_ar/features/Authentication/presentation/bloc/authentication_bloc.dart';
 import 'package:tup_ar/features/Authentication/presentation/widgets/authenticated/dashboard_drawer_header.dart';
 
@@ -13,15 +15,26 @@ class DashboardDrawer extends StatelessWidget {
         children: [
           const DashboardDrawerHeader(),
           ListTile(
+            title: const Text('FAQs'),
+            trailing: const Icon(
+              Icons.question_mark_rounded,
+            ),
+            style: ListTileStyle.drawer,
+            onTap: () {
+              AppRouter.push(AppRoutes.faqs);
+            },
+          ),
+          ListTile(
             title: const Text('Logout'),
+            trailing: const Icon(
+              Icons.logout,
+            ),
+            style: ListTileStyle.drawer,
             onTap: () {
               GetIt.instance<AuthenticationBloc>().add(
                 LogoutEvent(),
               );
             },
-            trailing: const Icon(
-              Icons.logout,
-            ),
           ),
         ],
       ),
